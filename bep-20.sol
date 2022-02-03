7e8f8488c1fc1fb3ba378b78f367782b8249ddc2
Luciano Zavala <42782441+luccianozz@users.noreply.github.com>
2022-02-02 20:04:24 -0400

    Create bep-20.sol

---
+++ bep-20.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract CRAS360 is ERC20{
+    constructor(uint256 initialSupply) public ERC20 ("CRAS360", "C360"){
+        _mint(msg.sender,initialSupply);
+    }
+}
+
